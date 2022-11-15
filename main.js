1d2ca0fa031d0e6a45938ea51f85bbc60ab4fbb1
cwest9400 <chrstphrwest@gmail.com>
2022-11-15 11:29:35 -0500

    planning -pseudo code & wireframes

---
+++ main.js
@@ -0,0 +1,25 @@
+//deck shuffles 52 cards
+//deck is split in half (26 cards) for each player
+//user clicks a 'flip' button
+//top card from user's deck is revealed
+//opponents top card is revealed from their deck.
+//the two revealed cards are compaired
+//the higher value card wins (decision)
+//the winning player receives both cards in the 'won cards' area
+//repeat
+//TIE - in case of tie, WaR is initiated.
+//The 'ante' button is activated.
+//the user must click the 'ante' button to add 3 cards, face down, to the ante pile.
+//the 'ante' button is disabled
+//the 'flip' button is enabled
+//the user clicks the flip button.
+//the users top card is revealed from their deck
+//the opponents to card is revealed from their deck
+//the two revealed cards are compaired
+//the higher value card wins (decision)
+//the winning player receives both cards + cards from each player's ante area in the 'won cards' area
+//restart from beginning
+//when user's deck is empty - shuffle cards from ante pile and move to user's deck area
+//when opponents's deck is empty - shuffle cards from ante pile and move to opponent's deck area
+//when user has 0 cards in deck or in ante pile, they lose.
+//when opponent has 0 cards in deck or in ante pile, they lose.
