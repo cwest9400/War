b4ed5699a15a17a51bbbb2b1d6ae3b5d981adc80
cwest9400 <chrstphrwest@gmail.com>
2022-11-16 12:32:59 -0500

    shuffle deck, defined opponent obj/player obj, assign half deck to opp/player obj

--- main.js
+++ main.js
@@ -1,34 +1,56 @@
-//1. make a deck - any deck
-//make array of values
-//loop through and push to new array (deck)
-let cards = ['a', 'b', 'c', 'd', 'e', '1', 'a']
-let cardType = ['red', 'black']
-let deck = []
-
-for (i = 0; i < cards.length; i++) {
-    for (j = 0; j < cardType.length; j++) {
-        deck.push(cards[i] + cardType[j])
-
-    }
-    // console.log(cards[i])
-
-}
-console.log(deck)
-
-
 //1a.
 //create a 52 card deck - 4 suits X 12 card types
-let 
 //make array of suits(hearts,clubs,spades,diamonds) & card values (2 thru Ace)
 //loop through and push to array (deck)
-
-//2.shuffle deck - 
+function makeDeck() {
+    let cardType = ['spade', 'club', 'diamond', 'heart']
+    let cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    let deck = []
+    for (i = 0; i < cardType.length; i++) {
+        for (j = 0; j < cards.length; j++) {
+            deck.push(cards[j] + cardType[i])
+        }
+    }
+    return (deck)
+}
+//2.shuffle deck -
 //loop through deck and assign random positions?
+function shuffleUp(deck) {
+    for (i = 0; i < deck.length; i++) {
+        let randomCard = deck[i]
+        let randomIndex = Math.floor(Math.random() * deck.length)
+        deck[i] = deck[randomIndex]
+        deck[randomIndex] = randomCard
+    }
+}
+let shuffledDeck = makeDeck()
 
 
 //3. split deck push half to new array for each player?
 //define players - object?
+let opponent = {
+    name: "Opponent",
+    opponentDeck: [],
+}
+
+let player = {
+    name: "player",
+    playerDeck:[],
+}
+shuffleUp(shuffledDeck)
+for (i=0; i < shuffledDeck.length; i++){
+    if (i < shuffledDeck.length/2) {
+        player.playerDeck.push(shuffledDeck[i])
+    } else {
+        opponent.opponentDeck.push(shuffledDeck[i])
+    }
+}
+console.log(player.playerDeck)
+console.log(opponent.opponentDeck)
+
 
 //4. flip card - 
 //pop card from top of player deck array
 //push to center object array
+//5. compare cards -
+//make card rank object? need to rank every card (j==10,Q==11,K==12,A==13)
