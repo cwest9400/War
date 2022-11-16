fcb041fc3c25ede68f47b288c70346365ddd10f5
cwest9400 <chrstphrwest@gmail.com>
2022-11-16 09:01:12 -0500

    psuedo making deck

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+//1. make a deck - any deck
+//make array of values
+//loop through and push to new array (deck)
+//1a.
+//create a 52 card deck - 4 suits X 12 card types
+//make array of suits(hearts,clubs,spades,diamonds) & card values (2 thru Ace)
+//loop through and push to array (deck)
+
+//2.shuffle deck - 
+//loop through deck and assign random positions?
 
